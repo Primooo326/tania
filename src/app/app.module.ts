72050f0a3787b2672fe1701af88c5afffaaa0337
primooo <juanskate326@gmail.com>
2022-09-17 17:40:31 -0500

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LinkComponent } from './pages/link/link.component';
 import { HomeComponent } from './pages/home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, NavbarComponent, LinkComponent, HomeComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/link/link.component.ts
+++ src/app/pages/link/link.component.ts
@@ -1,14 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
   styleUrls: ['./link.component.css'],
 })
 export class LinkComponent implements OnInit {
-  constructor() {}
+  constructor(private httpClient: HttpClient) {}
+
+  socialMedia = [
+    'https://www.instagram.com/coachtaniatorres/',
+    'https://www.linkedin.com/in/tania-tatiana-torres-real-59082524a/',
+    'https://wa.me/573138703512',
+  ];
 
   ngOnInit(): void {
     console.log('object');
+  }
+  goToSocialNetwork(socialMedia: string) {
+    return (window.location.href = socialMedia);
   }
 }
