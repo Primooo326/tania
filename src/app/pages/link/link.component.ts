b9015a4050fd23cb7123d6e1c766f09d5055458b
juan morales <juan.morales@tsicol.com>
2026-05-31 18:52:09 -0500

    Actualizar marca a FUNIDEF EXPRÉSATE, enlaces de WhatsApp y configurar archivo config.json

--- src/app/pages/link/link.component.ts
+++ src/app/pages/link/link.component.ts
@@ -1,17 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-link',
   templateUrl: './link.component.html',
   styleUrls: ['./link.component.css'],
 })
 export class LinkComponent implements OnInit {
-  constructor() {}
+  config: any = {
+    brandName: 'FUNIDEF EXPRÉSATE',
+    subtitle: 'Consultora en desarrollo del ser',
+    slogan: 'Actúa ya, elige tú buen vivir',
+    instagramUrl: 'https://www.instagram.com/coachtaniatorres/',
+    linkedinUrl: 'https://www.linkedin.com/in/tania-tatiana-torres-real-59082524a/',
+    whatsappUrl: 'https://wa.me/573138703512'
+  };
 
-
+  constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
-    console.log('object3');
+    this.http.get('assets/config.json').subscribe({
+      next: (data) => {
+        this.config = data;
+      },
+      error: (err) => {
+        console.error('Error loading config.json, using defaults', err);
+      }
+    });
   }
+
   goToSocialNetwork(socialMedia: string) {
     return (window.location.href = socialMedia);
   }
