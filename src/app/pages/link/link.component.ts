9b4c3688c856f8f985593c2dab3a45dc997b6c67
Primooo <juanskate326@gmail.com>
2026-07-08 16:51:09 -0500

    refactor: update application metadata, add TikTok icon, and refresh main script bundle

--- src/app/pages/link/link.component.ts
+++ src/app/pages/link/link.component.ts
@@ -10,11 +10,12 @@ export class LinkComponent implements OnInit {
   config: any = {
     brandName: 'FUNIDEF EXPRÉSATE',
     subtitle: 'Corporación Fundaciones Unidas para el Desarrollo Familiar.',
-    slogan: 'Actúa hoy, elige tú buen vivir.',
+    slogan: 'Hoy actuó, elijo mi buen vivir.',
     logoUrl: 'assets/icons/LOGO%20FUNIDEF%20EXPRESATE%20SIN%20FONDO.png',
     instagramUrl: 'https://www.instagram.com/funidef_expresate?utm_source=qr&igsh=MTAwd3BsbmRyY25rOQ==',
     linkedinUrl: 'https://www.linkedin.com/in/tania-tatiana-torres-real-59082524a/',
-    whatsappUrl: 'https://wa.me/573138703512'
+    whatsappUrl: 'https://wa.me/573138703512',
+    tiktokUrl: 'https://www.tiktok.com/@academia.de.bienestar?_r=1&_t=ZS-97GS9lEpnVP'
   };
 
   constructor(private http: HttpClient) {}
